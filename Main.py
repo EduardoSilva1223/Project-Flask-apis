cfe90ba4b2bef0610993baed4183b8151d95e1cf
Yatobuz <eduardojesus128z@gmail.com>
2024-08-09 18:14:59 -0300

    fix: conection mongo

--- Main.py
+++ Main.py
@@ -1,20 +1,38 @@
 ###
 # importando bibliotecas
 ###
-from flask import Flask
+from flask import Flask, jsonify
 from flask_restful import Resource, Api
+from flask_mongoengine import MongoEngine
 
 
 # definindo variaveis globais
 
 app = Flask(__name__)
+
+app.config['MONGODB_SETTINGS'] = {
+    'db': 'users',
+    'host': 'mongodb',
+    'port': 27017,
+    'username': 'admin',
+    'password': 'admin'
+}
+
 api = Api(app)
+db = MongoEngine(app)
 
 
-# Definindo classes da api.
-class Users(Resource):
+class UserModel(db.Document):
+    cpf = db.StringField(required=True, unique=True)
+    email = db.StringField(required=True)
+    first_name = db.StringField(required=True)
+    last_name = db.EmailField(required=True)
+    birth_date = db.DateTimeField(requiered=True)
+
+
+class Users(Resource):   # Definindo classes da api.
     def get(self):
-        return {"message": "user1"}
+        return jsonify(UserModel.objects())
 
 
 class User(Resource):
